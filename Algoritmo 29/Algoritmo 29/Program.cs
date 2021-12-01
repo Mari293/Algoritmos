7fc528003782c668ae23cbd88e948a3c96d3f446
Mari293 <maricruzg020903@gmail.com>
2021-12-01 13:26:46 -0500

    Se crea algoritmo 29

---
+++ Algoritmo 29/Algoritmo 29/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algoritmo_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Definir si un número es capicúa");
+            Console.Write("Ingrese el número que desea saber si es capicúa: ");
+            int num = int.Parse(Console.ReadLine());
+            double centena = num/100;
+            double decena = (num%10)/10;
+            double unidad = (num%10)%10;
+
+            if (centena == unidad)
+            {
+                Console.WriteLine("El número " + num + " es capicúa");
+            }
+            else
+            {
+                Console.WriteLine("El número " + num + " no es capicúa");
+            }
+
+        }
+    }
+}
