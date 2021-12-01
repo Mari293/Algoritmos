fd5a79f892408106a5841f77c78edaf2d7e4b33a
Mari293 <maricruzg020903@gmail.com>
2021-12-01 15:38:13 -0500

    Se crea algoritmo 30

---
+++ Algoritmo 30/Algoritmo 30/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Algoritmo_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Año bisiesto");
+            Console.Write("Ingrese un año: ");
+            int anio = int.Parse(Console.ReadLine());
+            if(anio%4==0 && anio % 400 == 0)
+            {
+                if (anio % 100 != 0)
+                {
+                    Console.WriteLine("Bisiesto");
+                }
+                else
+                {
+                    Console.WriteLine("No es bisiesto");
+                }
+            }
+        }
+    }
+}
